4f0c188476e784511252cfc90020420f7fa41f16
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-10-04 14:02:47 -0400

    Adding arg checking for valid file paths and valid WAV files

--- p4500.py
+++ p4500.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 
 import sys
+import wave
 
 def check_args():
-  return len(sys.argv) == 3
+  try:
+    return len(sys.argv) == 3 and wave.open(sys.argv[1]) and wave.open(sys.argv[2])
+  except EOFError:
+    print 'ERROR Both files must be of WAV format'
+    return True
+  except IOError:
+    return False
 
 
 if not check_args():
