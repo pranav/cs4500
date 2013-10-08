6dd35bbb914765435bf332116cc0171fdb1294fe
Pranav Gandhi <pgandhi@hubspot.com>
2013-10-08 13:56:23 -0400

    Added a bare skeleton of the program.

--- p4500.py
+++ p4500.py
@@ -2,6 +2,8 @@
 
 import sys
 import wave
+import numpy
+
 
 def check_args():
   try:
@@ -13,8 +15,42 @@ def check_args():
     return False
 
 
-if not check_args():
-  sys.stderr.write('ERROR Usage: ./p4500 <path> <path>\n')
-  sys.exit(1)
- else:
-  sys.exit(0)
+# Normalize to single channel WAV
+# Normalize temp to 100 bpm
+# Returns a file path string, example: "/tmp/newfile.wav"
+def normalize_wave_file(wavfile):
+  pass
+
+
+# numpy.fft should break the file into chunks and perform an fft
+# return the array/fft
+def get_fft(wavfile):
+  pass
+
+# Compare the 2 FFTs using crazy linear distance thingys
+#TODO: Better comment
+def match_comparator(wav1, wav2):
+  pass
+
+
+def main():
+  # Check arguments and check if WAV file
+  if not check_args():
+    sys.stderr.write('ERROR Usage: ./p4500 <path> <path>\n')
+    sys.exit(1)
+
+  # Returns a file path, "/tmp/newfile.wav"
+  wav1 = normalize_wave_file(sys.argv[1])
+  wav2 = normalize_wave_file(sys.argv[2])
+
+  fft_wav1 = get_fft(wav1)
+  fft_wav2 = get_fft(wav2)
+
+  match = match_comparator(fft_wav1, fft_wav2)
+
+  if match:
+    print "MATCH"
+  else:
+    print "NO MATCH"
+
+main()
