e34500a782443147093af067ea970605aafd0727
Daniel Moran <danxmoran@gmail.com>
2013-10-08 13:31:51 -0400

    Print errors to stderr

--- p4500.py
+++ p4500.py
@@ -7,14 +7,14 @@ def check_args():
   try:
     return len(sys.argv) == 3 and wave.open(sys.argv[1]) and wave.open(sys.argv[2])
   except EOFError:
-    print 'ERROR Both files must be of WAV format'
+    sys.stderr.write('ERROR Both files must be of WAV format\n')
     return True
   except IOError:
     return False
 
 
 if not check_args():
-  print 'ERROR Usage: ./p4500 <path> <path>'
+  sys.stderr.write('ERROR Usage: ./p4500 <path> <path>\n')
 
 
 sys.exit(0)
